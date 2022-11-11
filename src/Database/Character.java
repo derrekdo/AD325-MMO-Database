fb9e2a51fb44c72d55a0545483247c02d8a6f6e8
derrekdo <derrekdo123@gmail.com>
2022-11-10 23:03:33 -0800

    Added methods to Character class: constructor, heal, injure, toString

--- src/Database/Character.java
+++ src/Database/Character.java
@@ -1,4 +1,48 @@
 package Database;
 
 public class Character {
+    private String name;
+    private int height;
+    private int weight;
+    private double moralAlignment;
+    private double healthPoints = 100.0;
+
+    public Character(String name, int height, int weight, double moralAlignment){
+        if(moralAlignment > 1){
+            moralAlignment = 1;
+        }else if(moralAlignment < -1){
+            moralAlignment = -1;
+        }
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+        this.moralAlignment = moralAlignment;
+    }
+
+    public String heal(double healthPoints){
+        this.healthPoints += healthPoints;
+        if(this.healthPoints > 100){
+            this.healthPoints = 100;
+        }
+        return "Healing " + name + " for: " + healthPoints;
+    }
+
+    public String injure(double healthPoints){
+        this.healthPoints += healthPoints;
+        if(this.healthPoints < 0){
+            this.healthPoints = 0;
+        }
+        return "Damaging " + name + " for: " + healthPoints;
+    }
+
+    public String toString(){
+        StringBuilder characterSheet = new StringBuilder("\n\t\tCharacter Sheet\n");
+        characterSheet.append("------------------------------\n");
+        characterSheet.append("Character Name: " + name);
+        characterSheet.append("\nHealth Points: " + healthPoints);
+        characterSheet.append("\nHeight: " + height);
+        characterSheet.append("\nWeight: " + weight);
+        characterSheet.append("\nAlignment: " + moralAlignment);
+        return characterSheet + "";
+    }
 }
--- src/Database/Main.java
+++ src/Database/Main.java
@@ -1,4 +1,8 @@
 package Database;
 
 public class Main {
+    public static void main(String[] args) {
+        Character mage1 = new Character("Derk", 42, 150, 1.0);
+        System.out.println(mage1);
+    }
 }
